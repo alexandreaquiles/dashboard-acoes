7cc2382da0c03cb497b2af4a924a21af8c930967
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-07-04 17:59:11 -0300

    1 - adicionando título

--- Dashboard.py
+++ Dashboard.py
@@ -2,5 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.title('Dashboard de Ações')
+
 df_ibov = pd.read_csv('../../IBOV-2024-07-04-top10-participacao.csv', parse_dates=True, index_col='Date')
 st.dataframe(df_ibov, use_container_width=True)
