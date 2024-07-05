53209af786b27648933a2b8b1f16fbd82e2e4aac
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-07-04 21:03:03 -0300

    1 - tratando formato de datas

--- Dashboard.py
+++ Dashboard.py
@@ -4,5 +4,7 @@ import plotly.express as px
 
 st.title('Dashboard de Ações')
 
-df_ibov = pd.read_csv('../../IBOV-2024-07-04-top10-participacao.csv', parse_dates=True, index_col='Date')
-st.dataframe(df_ibov, use_container_width=True)
+df_ibov = pd.read_csv('../../IBOV-top10.csv', parse_dates=True, index_col='Date')
+st.dataframe(df_ibov, use_container_width=True, column_config={
+    "Date": st.column_config.DatetimeColumn(format="DD/MM/YYYY"
+)})
